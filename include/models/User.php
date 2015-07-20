0e426043cb5e40b592d4cc20f5c8a33255d30868
Carl Fink <cafink@gmail.com>
2015-07-19 22:52:38 -0500

    Add function to find coworkers.

--- include/models/User.php
+++ include/models/User.php
@@ -33,6 +33,28 @@ class User extends BaseRow {
 			throw new EmployeeException(get_class($this) . ' record #' . $id . ' is not an employee');
 		return $user;
 	}
+
+	function coworkers () {
+
+		// For each shift, find all concurrent shifts,
+		// then add the IDs of those shifts' employees
+		// to the $coworker_ids array (if they haven't
+		// already been added).
+		$coworker_ids = array();
+		foreach ($this->shifts as $shift) {
+			foreach ($shift->concurrentShifts() as $concurrent_shift) {
+				if (!empty($concurrent_shift->employee_id) && !in_array($concurrent_shift->employee_id, $coworker_ids))
+					$coworker_ids[] = $concurrent_shift->employee_id;
+			}
+		}
+
+		// Get the actual User object for each ID.
+		$coworkers = array();
+		foreach ($coworker_ids as $id)
+			$coworkers[] = $this->getEmployee($id);
+
+		return $coworkers;
+	}
 }
 
 function UserTable () {
