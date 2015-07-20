e7af7f82365ade9ef6829e2b7c02e336acf12bb1
Carl Fink <cafink@gmail.com>
2015-07-20 18:21:12 -0500

    Require specific user roles.

--- include/controllers/EmployeeController.php
+++ include/controllers/EmployeeController.php
@@ -5,23 +5,31 @@ include_once 'models/User.php';
 class EmployeeController extends ApplicationController {
 
 	function shifts ($coords) {
-		$this->shifts = UserTable()->getEmployee($coords['id'])->shifts;
-		$this->renderWithoutLayout();
+		if ($this->requestorRole('employee')) {
+			$this->shifts = UserTable()->getEmployee($coords['id'])->shifts;
+			$this->renderWithoutLayout();
+		}
 	}
 
 	function coworkers ($coords) {
-		$this->coworkers = UserTable()->getEmployee($coords['id'])->coworkers();
-		$this->renderWithoutLayout();
+		if ($this->requestorRole('employee')) {
+			$this->coworkers = UserTable()->getEmployee($coords['id'])->coworkers();
+			$this->renderWithoutLayout();
+		}
 	}
 
 	function summary ($coords) {
-		$this->summary = UserTable()->getEmployee($coords['id'])->summary();
-		$this->renderWithoutLayout();
+		if ($this->requestorRole('employee')) {
+			$this->summary = UserTable()->getEmployee($coords['id'])->summary();
+			$this->renderWithoutLayout();
+		}
 	}
 
 	function managers ($coords) {
-		$this->shifts = UserTable()->getEmployee($coords['id'])->shifts;
-		$this->renderWithoutLayout();
+		if ($this->requestorRole('employee')) {
+			$this->shifts = UserTable()->getEmployee($coords['id'])->shifts;
+			$this->renderWithoutLayout();
+		}
 	}
 }
 
