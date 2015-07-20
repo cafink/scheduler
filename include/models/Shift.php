32c9d8d48385a915f56870718b79d75ce6900859
Carl Fink <cafink@gmail.com>
2015-07-19 22:44:55 -0500

    Add function to find concurrent shifts.

--- include/models/Shift.php
+++ include/models/Shift.php
@@ -12,6 +12,26 @@ class Shift extends BaseRow {
 			$errors['end'] = 'End time must be specified.';
 		return $errors;
 	}
+
+	function concurrentShifts () {
+
+		$shifts = $this->find(array(
+			'where' => 'ID <> ?',
+			'params' => array($this->id)
+		));
+
+		$concurrent_shifts = array();
+		foreach ($shifts as $shift) {
+
+			// Two time periods overlap if the first
+			// starts before the second ends, and ends
+			// after the second starts.
+			if (($shift->start_time < $this->end_time) && ($shift->end_time > $this->start_time))
+				$concurrent_shifts[] = $shift;
+		}
+
+		return $concurrent_shifts;
+	}
 }
 
 function ShiftTable () {
